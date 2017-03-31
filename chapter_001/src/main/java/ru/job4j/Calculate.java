a16dd8d9ca4351ee9d4974141da68003447da685
Andrey Baranov <tread_lightly@list.ru>
2017-03-31 08:43:56 +0300

    Configure IDEA

---
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -0,0 +1,15 @@
+package ru.job4j;
+/**
+ * @author Andrey Baranov
+ * @version $Id$
+ * @since 0.1
+ */
+public class Calculate {
+	/**
+	 * method determines the entry point to the program.
+	 *@param args - parameters
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
