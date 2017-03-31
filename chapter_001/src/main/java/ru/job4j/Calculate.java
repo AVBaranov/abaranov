c5dff1f15be966af4398db3bf56f2f7fd210e1be
Andrey Baranov <tread_lightly@list.ru>
2017-03-31 10:20:59 +0300

    edit

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -10,6 +10,6 @@ public class Calculate {
 	 *@param args - parameters
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello World");
 	}
 }
