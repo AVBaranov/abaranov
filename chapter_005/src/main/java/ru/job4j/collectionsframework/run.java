59c37c02c262bc3b59e99603935fc59dd5694f9d
Andrey Baranov <tread_lightly@list.ru>
2017-06-08 09:12:36 +0300

    improve set

--- chapter_005/src/main/java/ru/job4j/collectionsframework/run.java
+++ chapter_005/src/main/java/ru/job4j/collectionsframework/run.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 import ru.job4j.collectionsframework.iterator.EvenNumbers;
 import ru.job4j.collectionsframework.map.Hashmap;
+import ru.job4j.collectionsframework.set.ArraySet;
 import ru.job4j.collectionsframework.tree.Tree;
 
 /**
@@ -174,6 +175,68 @@ class cl3 implements Iterator {
     }
 }
 
+class test<E> {
+
+    public Object[] array;
+
+    public boolean binarySearch(E value, Object[] array) {
+        if (search(value, array, 0, array.length - 1) != -1) {
+            return true;
+        }
+        return false;
+    }
+
+    private int search(E val, Object[] arr, int lo, int hi) {
+        if (lo > hi) return -1;
+        int mid = lo + (hi - lo) / 2;
+        if (val.hashCode() < arr[mid].hashCode()) {
+            return search(val, arr, lo, mid - 1);
+        } else if (val.hashCode() > arr[mid].hashCode()) {
+            return search(val, arr, mid +1, hi);
+        } else return mid;
+    }
+
+    void mergeSort() {
+        mergeSort(0, array.length);
+    }
+
+    void mergeSort(int low, int high) {
+        if (low + 1 < high) {
+            int mid = (low + high) >>> 1;
+            mergeSort(low, mid);
+            mergeSort(mid, high);
+
+            int size = high - low;
+            Object[] b = new Object[size];
+            int i = low;
+            int j = mid;
+            for (int k = 0; k < size; k++) {
+                if (j >= high || i < mid && array[i].hashCode() < array[j].hashCode()) {
+                    b[k] = array[i++];
+                } else {
+                    b[k] = array[j++];
+                }
+            }
+            System.arraycopy(b, 0, array, low, size);
+        }
+
+    }
+
+    void bubbleSort(Object[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i].hashCode() > array[j].hashCode()) {
+                    Object temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+    }
+
+}
+
+
 
 
 
@@ -237,14 +300,100 @@ public class run {
         Tree<String> ob = new Tree<>();
         ob.add("a", "b");
         ob.add("a", "c"); // код валится скорее всего из за того что не инициализируется поле children
-        ob.add("a", "d");
+        //ob.add("a", "d");
+
+
+
+        test test = new test();
+        Integer[] arr = { 1,12,23,34,55,61,67,77,88,89,101 };
+        System.out.println(test.binarySearch(101, arr));
+
+
+
+        String[] values = {"as", "qw", "az", "rt", "fg"};
+        for (String value : values) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+
+
+            for(int i = 0; i < values.length - 1; i++) {
+                for (int j = i + 1; j < values.length - 1; j++) {
+                    if (values[j].hashCode() > values[j + 1].hashCode()) {
+                        String temperate = values[j + 1];
+                        values[j + 1] = values[j];
+                        values[j] = temperate;
+                    }
+                }
+            }
+        for (String value : values) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+        System.out.println(test.binarySearch("qw", values));
+        Random generator = new Random();
+        Integer[] ar = new Integer[20];
+        for (int i = 0; i < ar.length; i++) {
+            ar[i] = generator.nextInt(10);
+        }
+        for (Integer value: ar) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
 
 
 
 
+        test.array = new Integer[20];
+        for (int i = 0; i < test.array.length; i++) {
+            test.array[i] = generator.nextInt(10);
+        }
+        for (int i = 0; i < test.array.length; i++) {
+            System.out.print(test.array[i] + " ");
+        }
+        System.out.println();
+        test.mergeSort();
+        for (int i = 0; i < test.array.length; i++) {
+            System.out.print(test.array[i] + " ");
+        }
+        System.out.println();
+        ArraySet<Integer> ars = new ArraySet<>();
+        long begin = System.currentTimeMillis();
+        for (int i = 0; i < 20; i++) {
+            ars.add(generator.nextInt(10));
+        }
+        long end = System.currentTimeMillis();
+        System.out.println(end - begin);
+
+        while (ars.hasNext()) {
+            System.out.print(ars.next() + " ");
+        }
+        System.out.println();
+
+        ArraySet<Integer> ars2 = new ArraySet<>();
+        long begin2 = System.currentTimeMillis();
+        for (int i = 0; i < 20; i++) {
+            ars2.oldAAdd(generator.nextInt(10));
+        }
+        long end2 = System.currentTimeMillis();
+        System.out.println(end2 - begin2);
+
+        while (ars2.hasNext()) {
+            System.out.print(ars2.next() + " ");
+        }
+
+
+
+
+        System.out.println();
 
 
     }
+
+
+
+
+
 
     int[] findnaturalnumbers(int[] sourcearray) {
 
--- chapter_005/src/main/java/ru/job4j/collectionsframework/set/ArraySet.java
+++ chapter_005/src/main/java/ru/job4j/collectionsframework/set/ArraySet.java
@@ -7,6 +7,7 @@ import java.util.Iterator;
  * Created by Андрей on 22.05.2017.
  */
 public class ArraySet<E> implements Iterator<E> {
+
     private static final Object[] EMPTY_ELEMENTDATA = {};
 
     private Object[] values;
@@ -21,7 +22,105 @@ public class ArraySet<E> implements Iterator<E> {
         this.values = EMPTY_ELEMENTDATA;
     }
 
+    /**
+     * method adds elements to collection.
+     *@param value - adding element
+     *@return true if element was added, false otherwise
+     */
     public boolean add(E value) {
+
+        if (notExistDuplicate(value)) {
+            count++;
+            if (values.length <= count) {
+                int temp = values.length + 1;
+                values = Arrays.copyOf(values, temp);
+            }
+            values[index++] = value;
+            sortByHashCode();
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * method checking array for duplicates.
+     *@param val - element checking for duplicate
+     *@return true if there is no duplicates in array
+     */
+    boolean notExistDuplicate(E val) {
+        if (binarySearch(val, values)) return false;
+        return true;
+    }
+
+    /**
+     * method searching duplicates.
+     *@param val - element checking for duplicate
+     *@param arr - source array
+     *@return true if duplicate detected
+     */
+    public boolean binarySearch(E val, Object[] arr) {
+        if (search(val, arr, 0, arr.length - 1) != -1) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * method searching duplicates in array.
+     *@param val - element checking for duplicate
+     *@param arr - source array
+     *@param lo - index of first element in array
+     *@param hi - index of last element in array
+     *@return index of duplicate element
+     */
+    private int search(E val, Object[] arr, int lo, int hi) {
+        if (lo > hi) return -1;
+        int mid = lo + (hi - lo) / 2;
+        if (val.hashCode() < arr[mid].hashCode()) {
+            return search(val, arr, lo, mid - 1);
+        } else if (val.hashCode() > arr[mid].hashCode()) {
+            return search(val, arr, mid +1, hi);
+        } else return mid;
+    }
+
+    /**
+     * method for sort elements in array by hashCode.
+     */
+    void sortByHashCode() {
+        mergeSort(0, values.length);
+    }
+
+    /**
+     * merge sort.
+     *@param low - index of first element in source array
+     *@param high - index of last element in source array
+     */
+    void mergeSort(int low, int high) {
+            if (low + 1 < high) {
+                int mid = (low + high) >>> 1;
+                mergeSort(low, mid);
+                mergeSort(mid, high);
+
+                int size = high - low;
+                Object[] b = new Object[size];
+                int i = low;
+                int j = mid;
+                for (int k = 0; k < size; k++) {
+                    if (j >= high || i < mid && values[i].hashCode() < values[j].hashCode()) {
+                        b[k] = values[i++];
+                    } else {
+                        b[k] = values[j++];
+                    }
+                }
+                System.arraycopy(b, 0, values, low, size);
+            }
+    }
+
+    /**
+     * old method adding element to collection.
+     *@param value - adding element
+     */
+    public boolean oldAAdd(E value) {
         for (int i = 0; i < values.length; i++) {
             if (values[i].equals(value)) {
                 return false;
