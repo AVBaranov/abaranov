bd4fc69ae09e65ae86fff61f306a8cbb62e08e29
Andrey Baranov <tread_lightly@list.ru>
2017-12-27 16:22:35 +0300

    Реализовать модели и связи для задание SQL хранилище машин

--- chapter_010/src/main/java/hibernate/todolist/Main.java
+++ chapter_010/src/main/java/hibernate/todolist/Main.java
@@ -14,23 +14,30 @@ import java.util.List;
 public class Main {
     public static void main (String[] args) {
 //        ItemStorage.getInstance().add(new Item(1, "newitem", "21 december", true));
+
+        Item item = new Item();
+        item.setId(11);
+        item.setDescr("test");
+        item.setCreated("20:23");
+        item.setDone(false);
          SessionFactory factory = new Configuration().configure().buildSessionFactory();
         Session session = factory.openSession();
         session.beginTransaction();
-        Item item = new Item();
-        item.setId(2);
-        item.setDescr("newtest");
-        item.setCreated("16:04564567");
-        item.setDone(false);
+
         /*List<Item> items = session.createQuery("from Item").list();
         for (Item item : items) {
             System.out.println(item.getDescr());
         }*/
         session.save(item);
+        item.setDescr("sdtgsdrg");
+        item.setCreated("sdf");
+        item.setDone(true);
+//        session.update(item);
+        session.getTransaction().commit();
 
 
 //        System.out.println(session.createQuery("from Item").list());
-        session.getTransaction().commit();
+
         session.close();
         factory.close();
     }
