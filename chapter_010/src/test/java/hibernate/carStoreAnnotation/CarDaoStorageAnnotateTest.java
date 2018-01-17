a2d4a517232e803a9c35f3d0c69fe736521cfd92
Andrey Baranov <tread_lightly@list.ru>
2018-01-17 13:58:47 +0300

    Добавить интеграционные тесты для площадок машин

--- chapter_010/src/test/java/hibernate/carStoreAnnotation/CarDaoStorageAnnotateTest.java
+++ chapter_010/src/test/java/hibernate/carStoreAnnotation/CarDaoStorageAnnotateTest.java
@@ -24,4 +24,52 @@ public class CarDaoStorageAnnotateTest {
         List<Car> cars = carDaoStorageAnnotate.findByCredential("crossover", "auto", "x6");
         assertThat("x6", is(cars.get(0).getEngine().getModel()));
     }
+
+    @Test
+    public void whenAddNewEntityThenCanGetFieldOfItEntity() {
+        CarDaoStorageAnnotate carDaoStorageAnnotate = new CarDaoStorageAnnotate();
+        carDaoStorageAnnotate.deleteAll();
+        Engine engineTest = new Engine();
+        engineTest.setModel("x6");
+        Carbody carbodyTest = new Carbody();
+        carbodyTest.setType("crossover");
+        Transmission transmission = new Transmission();
+        transmission.setType("auto");
+        Car car = new Car("bmw", engineTest, carbodyTest, transmission, false);
+        carDaoStorageAnnotate.create(car);
+        assertThat("x6", is(carDaoStorageAnnotate.read().get(0).getEngine().getModel()));
+    }
+
+    @Test
+    public void whenDeleteAllFromTableThenNoMoreRowsInTheTable() {
+        CarDaoStorageAnnotate carDaoStorageAnnotate = new CarDaoStorageAnnotate();
+        Engine engineTest = new Engine();
+        engineTest.setModel("x6");
+        Carbody carbodyTest = new Carbody();
+        carbodyTest.setType("crossover");
+        Transmission transmission = new Transmission();
+        transmission.setType("auto");
+        Car car = new Car("bmw", engineTest, carbodyTest, transmission, false);
+        engineTest.setModel("q5");
+        Car car2 = new Car("audi", engineTest, carbodyTest, transmission, false);
+        carDaoStorageAnnotate.create(car);
+        carDaoStorageAnnotate.create(car2);
+        carDaoStorageAnnotate.deleteAll();
+        assertThat(0, is(carDaoStorageAnnotate.read().size()));
+    }
+
+    @Test
+    public void whenReadFromDatabaseTableThenCanGetAllEntities() {
+        CarDaoStorageAnnotate carDaoStorageAnnotate = new CarDaoStorageAnnotate();
+        Engine engineTest = new Engine();
+        engineTest.setModel("x6");
+        Carbody carbodyTest = new Carbody();
+        carbodyTest.setType("crossover");
+        Transmission transmission = new Transmission();
+        transmission.setType("auto");
+        Car car = new Car("bmw", engineTest, carbodyTest, transmission, false);
+        carDaoStorageAnnotate.create(car);
+        assertThat(car.getEngine().getModel(), is(carDaoStorageAnnotate.read().get(0).getEngine().getModel()));
+    }
+
 }
