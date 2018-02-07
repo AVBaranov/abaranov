e34c151b96f0301bf2c81a9b44c5c72017b27f7f
Andrey Baranov <tread_lightly@list.ru>
2018-02-07 11:09:22 +0300

    changes

---
+++ chapter_012/xml/src/main/java/spring/controllers/UserController.java
@@ -0,0 +1,31 @@
+package spring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import spring.models.User;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+
+/**
+ * Created by Андрей on 07.02.2018.
+ */
+@Controller
+public class UserController {
+
+    final List<User> users = new CopyOnWriteArrayList<>();
+
+    /*@RequestMapping(value = "/users", method = RequestMethod.GET)
+    public String showItems(ModelMap model) {
+        model.addAttribute("users", this.users);
+        return "users";
+    }
+
+    @RequestMapping(value = "/users", method = RequestMethod.GET)
+    public String addItem(@ModelAttribute User user) {
+        this.users.add(user);
+        return "redirect:users.do";
+    }*/
+}
+
---
+++ chapter_012/xml/src/main/java/spring/models/User.java
@@ -0,0 +1,7 @@
+package spring.models;
+
+/**
+ * Created by Андрей on 07.02.2018.
+ */
+public class User {
+}
